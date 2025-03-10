e5fea5aebd1d650de0116e91c56002c8c71ef443
shukal94 <sergey.shukalovich@gmail.com>
2025-03-10 22:18:51 +0100

    added db logging

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,22 +1,26 @@
+import logging
+
 COMPANY_NAME = "Solvd Inc."
 DATA_TO_INSERT = (4, 'Google', 30, 'Somewhere in Nowhere', 1000)
 
+LOGGER = logging.getLogger("test")
 
 def test_select_all(test_db_client):
+    LOGGER.info("Get all the companies and validate result is not empty")
     result = test_db_client.get_companies()
-    for row in result:
-        print(row)
     assert result is not None, "Query failed!"
 
 
 def test_select_by_name(test_db_client):
+    LOGGER.info(f"Get by name '{COMPANY_NAME}' and validate result is not empty")
     result = test_db_client.get_company_by_name(name=COMPANY_NAME)
-    print(result)
     assert result is not None, "Query failed!"
 
 
 def test_insert(test_db_client):
+    LOGGER.info("Creating a new company.")
     test_db_client.add_company(company=DATA_TO_INSERT)
+
+    LOGGER.info("Validating the new company created.")
     result = test_db_client.get_company_by_name("Google")
-    print(result)
     assert result is not None, "Query failed!"
