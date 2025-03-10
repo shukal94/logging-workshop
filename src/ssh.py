09a02dd2fc29cbb1cc0ce0d2553e7405af559641
shukal94 <sergey.shukalovich@gmail.com>
2025-03-10 21:30:27 +0100

    added logging for sftp

--- src/ssh.py
+++ src/ssh.py
@@ -1,4 +1,30 @@
+import logging
+
 import paramiko
+
+LOGGER = logging.getLogger("sftp")
+LOGGER.setLevel(logging.INFO)
+
+
+class LoggingSftpClient(paramiko.SFTPClient):
+    """
+    One of the possible approaches: decorator around SFTPClient.
+    Instantiation and signatures the same as for original. Uses just once.
+    """
+
+    def get(self, remotepath, localpath, callback=None, prefetch=True, max_concurrent_prefetch_requests=None):
+        LOGGER.info(f"Downloading {remotepath} to {localpath}")
+        return super().get(remotepath, localpath, callback)
+
+    def listdir(self, path: str = '.'):
+        LOGGER.info(f"Listing directory {path}")
+        ls_result = super().listdir(path)
+        LOGGER.info(f"Result: {ls_result}")
+        return ls_result
+
+    def put(self, localpath, remotepath, callback=None, confirm=True):
+        LOGGER.info(f"Uploading {localpath} to {remotepath}")
+        return super().put(localpath, remotepath, callback=None, confirm=True)
 
 
 class SshClient:
@@ -7,7 +33,7 @@ class SshClient:
     username: str
     password: str
     ssh: paramiko.SSHClient
-    sftp: paramiko.SFTPClient
+    sftp: LoggingSftpClient # just define it here
 
     def __init__(self, host: str, port: int, username: str, password: str):
         self.host = host
@@ -25,13 +51,14 @@ class SshClient:
             port=self.port,
             username=self.username,
             password=self.password,
-            allow_agent=False,
-            look_for_keys=False
+            allow_agent=False, # to skip key auth
+            look_for_keys=False # to skip key auth
         )
 
     def connect_sftp(self):
         if self.ssh:
-            self.sftp = self.ssh.open_sftp()
+            transport = self.ssh.get_transport() # as we cannot instantiate SFTPClient directly
+            self.sftp = LoggingSftpClient.from_transport(transport)
 
     def close(self):
         self.close_sftp()
@@ -53,4 +80,9 @@ class SshClient:
     def ls(self, remote_path: str):
         if self.sftp:
             return self.sftp.listdir(path=remote_path)
+        raise RuntimeError("No active sftp session opened!")
+
+    def put(self, local_path: str, remote_path: str):
+        if self.sftp:
+            return self.sftp.put(localpath=local_path, remotepath=remote_path)
         raise RuntimeError("No active sftp session opened!")
--- tests/test_sftp.py
+++ tests/test_sftp.py
@@ -1,19 +1,45 @@
+import logging
 import os
+import src.utils as utils
+from datetime import datetime
+from tests.conftest import DOWNLOADS_PATH, USER_TEMPLATE_PATH, TEST_FILES_PATH
 
 REMOTE_ROOT_PATH = "/"
-LOCAL_DOWNLOADS_PATH = ".testdata/downloads"
+REMOTE_EXAMPLES_PATH = "/expamples"
 FILENAME = "readme.txt"
+
+LOGGER = logging.getLogger("test") # logger object is a singleton, we can use one of the loggers defined in logging.ini
+SAMPLE_NAME = "John"
+SAMPLE_OCCUPATION = "SDET 4"
+SAMPLE_ADDRESS = "74 Kostavas Tbilisi 01-411, Georgia"
 
 
 def test_ls(sftp_client):
+    LOGGER.info("Validating root directory is not empty.")
     ls_result = sftp_client.ls(remote_path=REMOTE_ROOT_PATH)
     assert len(ls_result) != 0, "Nothing in root."
 
 
 def test_get(sftp_client):
-    path_to_download = f"{LOCAL_DOWNLOADS_PATH}/{FILENAME}"
+    LOGGER.info("Validating file download.")
+    path_to_download = f"{DOWNLOADS_PATH}/{FILENAME}"
     sftp_client.get(
         remote_path=f"{REMOTE_ROOT_PATH}/{FILENAME}",
         local_path= path_to_download
     )
     assert os.path.exists(path_to_download), "Download failed."
+
+
+def test_put(sftp_client):
+    LOGGER.info(f"Generating a new user from template {USER_TEMPLATE_PATH}.")
+    content = utils.read_from_file(USER_TEMPLATE_PATH)
+    content = content.format(name=SAMPLE_NAME, occupation=SAMPLE_OCCUPATION, address=SAMPLE_ADDRESS)
+
+    filename = f"user_{datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f')}.txt"
+    path_to_save = f"{TEST_FILES_PATH}/{filename}"
+    LOGGER.info(f"Saving a new user data into {path_to_save}.")
+    utils.write_to_file(content, path_to_save)
+
+    LOGGER.info("Uploading file and validating success upload.")
+    sftp_client.put(local_path=path_to_save, remote_path=REMOTE_EXAMPLES_PATH)
+    assert filename in sftp_client.ls(REMOTE_EXAMPLES_PATH), f"File {filename} was not uploaded."
