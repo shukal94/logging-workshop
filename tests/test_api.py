2e35a2c38a77eb02a82b6b903edbbc465ac0ed6b
shukal94 <sergey.shukalovich@gmail.com>
2025-03-12 00:08:41 +0100

    api logging

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,25 @@
+import logging
+
 POST_ID = 1
 USER_ID = 1
 
+LOGGER = logging.getLogger("test")
+
 
 def test_posts(json_placeholder_api):
+    LOGGER.info("Get all posts")
     json = json_placeholder_api.get_posts()
     assert json is not None, "Nothing has been returned."
 
 
 def test_posts_by_user_id(json_placeholder_api):
+    LOGGER.info(f"Get all posts by {USER_ID}")
     json = json_placeholder_api.get_posts(user_id=USER_ID)
     assert json is not None, "Nothing has been returned."
 
 
 def test_post_by_id(json_placeholder_api):
+    LOGGER.info(f"Get post by id={POST_ID}")
     json = json_placeholder_api.get_post_by_id(POST_ID)
     assert json is not None, "Nothing has been returned."
 
@@ -23,6 +30,7 @@ def test_add_post(json_placeholder_api):
         "body": "Sample body",
         "userId": USER_ID
     }
+    LOGGER.info(f"Create a new post.")
     json = json_placeholder_api.create_post(payload=payload)
     assert json["title"] == payload["title"], "Resource was not created."
 
@@ -34,6 +42,7 @@ def test_update_post(json_placeholder_api):
         "body": "Sample body",
         "userId": USER_ID
     }
+    LOGGER.info(f"Update post with id={POST_ID}")
     json = json_placeholder_api.update_post(post_id=POST_ID, payload=payload)
     assert json["title"] == payload["title"], "Resource was not updated."
 
@@ -42,10 +51,12 @@ def test_update_part_of_post(json_placeholder_api):
     payload = {
         "title": "Sample title"
     }
+    LOGGER.info(f"Update post with id={POST_ID}")
     json = json_placeholder_api.patch_post(post_id=POST_ID, payload=payload)
     assert json["title"] == payload["title"], "Resource was not updated."
 
 
 def test_delete_post(json_placeholder_api):
+    LOGGER.info(f"Update post with id={POST_ID}")
     json = json_placeholder_api.delete_post(post_id=POST_ID)
     assert json is not None, "Resource was not deleted."
